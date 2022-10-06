45e56445e6496efc43c6522bd61e0e931631d466
Maxwell Seybert <maxwellseybert@gmail.com>
2022-10-05 19:15:39 -0600

    Moved some folders, trying to add users

--- Project6/Superhero/hero/urls.py
+++ Project6/Superhero/hero/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .views import HeroCreateView, HeroDeleteView, HeroDetailView, HeroListView, HeroUpdateView
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
 
@@ -8,5 +11,7 @@ urlpatterns = [
     path('add',             HeroCreateView.as_view(),  name='hero_add'),
     path('<int:pk>/',       HeroUpdateView.as_view(),  name='hero_edit'),
     path('<int:pk>/delete', HeroDeleteView.as_view(),  name='hero_delete'),
-
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
