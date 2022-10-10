15165c59b325fba3e11ba882246933a2fbe39943
Maxwell Seybert <maxwellseybert@gmail.com>
2022-10-10 16:10:40 -0600

    Fixed Redirect

--- Superhero/config/settings.py
+++ Superhero/config/settings.py
@@ -128,5 +128,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL="home"
+LOGIN_REDIRECT_URL = 'hero_list'
+LOGOUT_REDIRECT_URL='hero_list'
