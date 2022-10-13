deada0e5421c20f13f9fe7ef6e571945cbe03d6e
Maxwell <maxwellseybert@gmail.com>
2022-10-12 21:01:11 -0600

    Played with demo code, tried tests on superhero

---
+++ Old-Demos/Week8/TDD/blog/tests.py
@@ -0,0 +1,7 @@
+from django.test import SimpleTestCase, TestCase
+
+# Create your tests here.
+class BlogAppTest(SimpleTestCase):
+
+    def test_django(self):
+        self.assertTrue(False)
--- Superhero/hero/tests.py
+++ Superhero/hero/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
 
 # Create your tests here.
+class HeroAppTest(SimpleTestCase):
+    
+    def test_django(self):
+        page = "https://jellyfish-app-w75km.ondigitalocean.app/"
+        response = self.client.get(page)
+        self.assertEqual(response.status_code, 200)
